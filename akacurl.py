cd3f14a6a1454db9e135b9f4a53df8dfc20c74ac
Masahiko Kitamura <ktmrmshk@gmail.com>
2017-08-05 19:26:13 +0900

    mod:

--- akacurl.py
+++ akacurl.py
@@ -1,6 +1,8 @@
+#!/usr/local/bin/python3
 import requests
 from akamai.edgegrid import EdgeGridAuth, EdgeRc
 import json
+import sys
 
 edgerc=EdgeRc('.edgerc')
 baseurl='{}{}'.format('https://', edgerc.get('default', 'host'))
@@ -8,7 +10,8 @@ baseurl='{}{}'.format('https://', edgerc.get('default', 'host'))
 s = requests.Session()
 s.auth = EdgeGridAuth.from_edgerc(edgerc, 'default')
 
-req='{}{}'.format(baseurl, '/diagnostic-tools/v1/locations')
+#req='{}{}'.format(baseurl, '/diagnostic-tools/v1/locations')
+req='{}{}'.format(baseurl, sys.argv[1])
 r = s.get(req)
 
 print(req)

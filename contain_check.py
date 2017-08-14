bb118e95f02d33d4b8665653d995480a4183edc8
Masahiko Kitamura <ktmrmshk@gmail.com>
2017-08-14 10:10:58 +0900

    added contain_check tools

---
+++ contain_check.py
@@ -0,0 +1,27 @@
+import sys
+
+
+
+if __name__ == '__main__':
+  bigfile = sys.argv[1]
+  listfile = sys.argv[2]
+
+  with open(bigfile) as fbig:
+    bigstr=fbig.read()
+    with open(listfile) as flist:
+      cnt=0
+      for h in flist:
+        host = h.strip()
+        if host == '':
+          continue
+        cnt+=1
+        print(host)
+        if bigstr.find(host) == -1:
+          print('  {} is not contained in {}'.format(host, bigfile))
+        else:
+          print(' => OK')
+      else:
+        print('Done!')
+        print('{} hosts was checked.'.format(cnt))
+
+
